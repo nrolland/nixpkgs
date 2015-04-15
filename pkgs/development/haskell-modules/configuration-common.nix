d809975ae2b61bbfbab0c614f6e62e1b5b09d23f
Nicolas Rolland <nicolas.rolland@gmail.com>
2015-04-15 11:52:52 +0100

    shake haskell : disabling tests as it requires gcc, which fails

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -88,6 +88,10 @@ self: super: {
   # The test suite refers to its own library with an invalid version constraint.
   presburger = dontCheck super.presburger;
 
+  #no gcc in the test environment of nix, while shake wants it
+  shake = dontCheck super.shake;
+
+
   # Won't find it's header files without help.
   sfml-audio = appendConfigureFlag super.sfml-audio "--extra-include-dirs=${pkgs.openal}/include/AL";
 
